4ad8ab2cf315fd7c36ad9431a351d5a67b7a807f
고동현 <dhjk35@naver.com>
2022-07-01 17:51:31 +0900

    :wrench: Chore: eslint, stylelint, prettier 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+const createConfig = require('@titicaca/eslint-config-triple/create-config')
+
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  rules: {},
+}
