47769581d1f7d587f89f89e069b25c15958179c9
고동현 <dhjk35@naver.com>
2022-07-01 18:14:29 +0900

    :wrench: Chore: jest 설정

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: '.' })
+
+const customJestConfig = {
+  verbose: true,
+  collectCoverage: true,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(ts|tsx)$': ['ts-jest', { presets: ['next/babel'] }],
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
