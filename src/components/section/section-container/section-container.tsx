67cf823e5dad8a66855c736819f3042c4e67443f
고동현 <dhjk35@naver.com>
2022-07-04 17:00:55 +0900

    :sparkles: Feat: section-container 컴포넌트 구현

---
+++ src/components/section/section-container/section-container.tsx
@@ -0,0 +1,49 @@
+import styled, { css } from 'styled-components'
+
+import type { SectionBackgroundStyle } from 'types'
+
+interface SectionContainerProps {
+  backgroundStyle?: SectionBackgroundStyle
+}
+
+const SECTION_BACKGROUND_STYLES: Partial<
+  Record<SectionBackgroundStyle, ReturnType<typeof css>>
+> = {
+  default: css`
+    background-image: inherit;
+    background-repeat: inherit;
+    background-attachment: inherit;
+    background-origin: inherit;
+    background-clip: inherit;
+    background-color: inherit;
+  `,
+  primary: css`
+    background-color: ${({ theme }) => theme.color.brightGray};
+  `,
+  secondary: css`
+    background-color: ${({ theme }) => theme.color.deepBrightGray};
+  `,
+  toolbox: css`
+    background-color: ${({ theme }) => theme.color.mint};
+    background-image: url('https://triple.guide/intro/images/bg-section-06.svg');
+  `,
+  recruit: css`
+    background-color: ${({ theme }) => theme.color.blue};
+    background-image: url('https://triple.guide/intro/images/group-section-8.svg');
+  `,
+  footer: css`
+    background-color: ${({ theme }) => theme.color.gunmetal};
+  `,
+}
+
+const SectionContainer = styled.section<SectionContainerProps>`
+  position: relative;
+  background-position: center center;
+  background-size: cover;
+  overflow: hidden;
+
+  ${({ backgroundStyle }) =>
+    SECTION_BACKGROUND_STYLES[backgroundStyle || 'default']}
+`
+
+export default SectionContainer
