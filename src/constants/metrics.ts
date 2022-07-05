1d3d7616910c5419f0134fc3ce1ba22edf64eb6f
고동현 <dhjk35@naver.com>
2022-07-06 07:13:10 +0900

    :sparkles: Feat: metric 컴포넌트 구현

---
+++ src/constants/metrics.ts
@@ -0,0 +1,17 @@
+export const MetricItemList = [
+  {
+    metric: 700,
+    unit: '만 명의',
+    subject: '여행자',
+  },
+  {
+    metric: 100,
+    unit: '만 개의',
+    subject: '여행 리뷰',
+  },
+  {
+    metric: 460,
+    unit: '만 개의',
+    subject: '여행 일정',
+  },
+]
