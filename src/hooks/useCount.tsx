3f2f36044862d821f70b6e91736ca278409d32dc
고동현 <dhjk35@naver.com>
2022-07-06 07:09:17 +0900

    :sparkles: Feat: useCount 훅 구현

---
+++ src/hooks/useCount.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useCallback, useState } from 'react'
+
+import { easeOutExpo } from 'utils/easing'
+
+export const useCount = (end: number, start = 0, duration = 1500) => {
+  const [count, setCount] = useState<number>(start)
+
+  const frameRate = 1000 / 120
+  const totalFrame = Math.floor(duration / frameRate)
+
+  const increaseCount = useCallback(() => {
+    let countNumber = start
+
+    const counter = setInterval(() => {
+      let progress = ++countNumber / totalFrame
+      progress = Math.round(easeOutExpo(progress, start, end, duration / 1000))
+      setCount(progress)
+
+      if (progress === end) {
+        clearInterval(counter)
+      }
+    }, frameRate)
+  }, [start, end, duration, frameRate, totalFrame])
+
+  useEffect(() => {
+    increaseCount()
+  }, [increaseCount])
+
+  return count
+}
