478935450ef8e76673e7f935f559062116b3a9c3
고동현 <dhjk35@naver.com>
2022-07-06 07:14:38 +0900

    :white_check_mark: Test: metric 컴포넌트 테스트

---
+++ src/components/statistic-section/metrics/__test__/metric.test.tsx
@@ -0,0 +1,36 @@
+import { act } from 'react-dom/test-utils'
+
+import Metric from '../metric'
+
+import { render } from 'utils/test'
+
+describe('Metric', () => {
+  it('should be rendered', () => {
+    const { container } = render(<Metric />)
+
+    expect(container).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should be 0 when initial rendering of count values', () => {
+    const { getAllByText } = render(<Metric />)
+
+    expect(getAllByText('0')[0]).toBeInTheDocument()
+    expect(getAllByText('0')[1]).toBeInTheDocument()
+    expect(getAllByText('0')[2]).toBeInTheDocument()
+  })
+
+  it('should be updated the values of count after a time', () => {
+    jest.useFakeTimers()
+
+    const { getAllByText } = render(<Metric />)
+
+    act(() => {
+      jest.advanceTimersByTime(3000)
+    })
+
+    expect(getAllByText('700')[0]).toBeInTheDocument()
+    expect(getAllByText('100')[0]).toBeInTheDocument()
+    expect(getAllByText('460')[0]).toBeInTheDocument()
+  })
+})
