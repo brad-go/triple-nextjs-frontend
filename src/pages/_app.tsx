39da482f82cf173cc4392755bd8db57d9d7d3e69
고동현 <dhjk35@naver.com>
2022-07-07 01:10:37 +0900

    :sparkles: Feat: Seo 최적화

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title />
+        <title>트리플: 여행의 모든 것, 트리플로 한 번에</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,46 @@ class MyDocument extends Document {
   public render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="title"
+            content="트리플: 여행의 모든 것, 트리플로 한 번에"
+          />
+          <meta
+            name="description"
+            content="최저가 예약부터 여행 일정까지 앱 하나로 한 번에! / 항공권, 숙소, 투어·티켓 할인 / 제주도 포함 국내&amp;해외 도시별 여행정보 / 동선 보며 일정짜기 / 장소별 리뷰 / 가계부, 날씨, 길찾기"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover"
+          />
+          <meta
+            property="og:title"
+            content="트리플: 여행의 모든 것, 트리플로 한 번에"
+          />
+          <meta property="og:url" content="https://triple.guide/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="ko_KR" />
+          <meta
+            property="og:image"
+            content="https://assets.triple.guide/images/img-default-page-preview.png"
+          />
+          <meta property="og:image:width" content="1052" />
+          <meta property="og:image:height" content="1052" />
+          <meta
+            property="og:description"
+            content="최저가 예약부터 여행 일정까지 앱 하나로 간편하게 준비하세요!"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="https://triple.guide/icons/favicon-144x144.png"
+          />
+          <meta name="msapplication-TileColor" content="#1FC1B6" />
+          <link
+            rel="apple-touch-icon-precomposed"
+            href="https://triple.guide/icons/favicon-152x152.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
