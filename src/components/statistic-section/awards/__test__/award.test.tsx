4eed99bef4282afb0267ad448d5ec0a897f7aa70
고동현 <dhjk35@naver.com>
2022-07-06 16:12:54 +0900

    :white_check_mark: Test: award 컴포넌트 테스트 수정

--- src/components/statistic-section/awards/__test__/award.test.tsx
+++ src/components/statistic-section/awards/__test__/award.test.tsx
@@ -11,9 +11,30 @@ describe('Award', () => {
     expect(container).toMatchSnapshot()
   })
 
-  it('should be rendered', () => {
+  it('should be rendered with parameter', () => {
     const { getAllByRole } = render(<Award awards={AWARD_LIST} />)
 
     getAllByRole('listitem')
+  })
+
+  it('should have background image', () => {
+    const { getAllByRole } = render(<Award awards={AWARD_LIST} />)
+
+    const list = getAllByRole('listitem')
+
+    list.forEach((item, idx) => {
+      expect(item).toHaveStyle(
+        `background-image: url(${AWARD_LIST[idx].image})`,
+      )
+    })
+  })
+
+  it('should have appropriate text', () => {
+    const { getByText } = render(<Award awards={AWARD_LIST} />)
+
+    expect(getByText(/2018 구글 플레이스토어/)).toBeInTheDocument()
+    expect(getByText(/올해의 앱 최우수상 수상/)).toBeInTheDocument()
+    expect(getByText(/2018 애플 앱스토어/)).toBeInTheDocument()
+    expect(getByText(/오늘의 여행앱 선정/)).toBeInTheDocument()
   })
 })
